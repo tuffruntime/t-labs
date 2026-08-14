d8a9a85536e265933388cd7283c1873b8c38f5c6
tuffruntime <thachphudang@gmail.com>
2026-08-14 22:43:23 +0700

    To be a Rustacean!

---
+++ rust/hello.rs
@@ -0,0 +1,46 @@
+// This is a comment, and is ignored by the compiler.
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
+// shortcut.
+fn main() {
+    println!("Hello world!");
+    println!("I'm a Rustacean!");
+
+    /*println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alicia", "Shiki");
+    println!("{subject} {verb} {object}", subject="I", verb="am", object = "a Rustacean");
+    println!("Base 10: {0}\nBase 2: {1:b}\nBase 8: {2:o}\nBase 16: {3:x}", 69420, 69420, 69420, 69420);
+    println!("{name:x>10}", name="Richard");
+    println!("{name:x<10}", name="Remi");
+
+    // struct Structure(i32);
+    // println!("This struct `{}` won't print!", Structure(3));
+
+    println!("{:?}", 1234.4);
+    println!("{}", 1234.4);
+
+    let pi = 3.141592;
+    println!("{:.5}", pi);
+
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    #[derive(Debug)]
+    struct Deep(Structure);
+
+    println!("{:?}", Structure(122));
+    println!("{:?}", Deep(Structure(0)));
+
+    #[derive(Debug)]
+    struct Person<'a> {
+        name: &'a str,
+        age: u8,
+    }
+
+    let name = "Peterson";
+    let age = 27;
+    let peters = Person { name, age };
+
+    println!("{:#?}", peters);*/
+
+}
